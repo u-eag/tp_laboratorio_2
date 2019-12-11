5259ada2cff01f8d131a9111c2c4c2578747b93e
Anto <eagualco@protonmail.com>
2019-12-11 16:24:52 -0300

    update clase Producto

--- RecuperatoriosTP/TP2/Entidades/Producto.cs
+++ RecuperatoriosTP/TP2/Entidades/Producto.cs
@@ -9,41 +9,73 @@ namespace Entidades_2018
     /// <summary>
     /// La clase Producto no deberá permitir que se instancien elementos de este tipo.
     /// </summary>
-    public sealed class Producto
+    public abstract class Producto
     {
-        enum EMarca
-        {
-            Serenisima, Campagnola, Arcor, Ilolay, Sancor, Pepsico
-        }
-        EMarca marca;
-        string codigoDeBarras;
-        ConsoleColor colorPrimarioEmpaque;
+        #region Campos
+
+        private string codigoDeBarras;       
+        private EMarca marca;
+        private ConsoleColor colorPrimarioEmpaque;
+
+        #endregion
+
+        #region Propiedades
 
         /// <summary>
         /// ReadOnly: Retornará la cantidad de calorias
         /// </summary>
-        abstract short CantidadCalorias { get; set; }
+        protected abstract short CantidadCalorias { get; }
+
+        #endregion
+
+        #region Constructores
+
+        /// <summary>
+        /// Constructor que recibe los 3 parámetros del objeto
+        /// [codigoDeBarras] [marca] [colorPrimarioEmpaque]
+        /// </summary>
+        /// <param name="codigo"></param>
+        /// <param name="marca"></param>
+        /// <param name="color"></param>
+        public Producto(string codigo, EMarca marca, ConsoleColor color)
+        {
+            this.codigoDeBarras = codigo;
+            this.marca = marca;
+            this.colorPrimarioEmpaque = color;
+        }
+
+        #endregion
+
+        #region Métodos
 
         /// <summary>
         /// Publica todos los datos del Producto.
         /// </summary>
         /// <returns></returns>
-        sealed string Mostrar()
+        public virtual string Mostrar()
         {
-            return this;
+            return (string)this;
         }
 
-        private static explicit operator string(Producto p)
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="p"></param>
+        public static explicit operator string(Producto p)
         {
             StringBuilder sb = new StringBuilder();
 
-            sb.AppendLine("CODIGO DE BARRAS: {0}\r\n", p.codigoDeBarras);
-            sb.AppendLine("MARCA          : {0}\r\n", p.marca.ToString());
-            sb.AppendLine("COLOR EMPAQUE  : {0}\r\n", p.colorPrimarioEmpaque.ToString());
+            sb.AppendFormat("CODIGO DE BARRAS: {0}\r\n", p.codigoDeBarras);
+            sb.AppendFormat("MARCA          : {0}\r\n", p.marca.ToString());
+            sb.AppendFormat("COLOR EMPAQUE  : {0}\r\n", p.colorPrimarioEmpaque.ToString());
             sb.AppendLine("---------------------");
 
-            return sb;
+            return sb.ToString();
         }
+
+        #endregion
+
+        #region Sobrecarga de Operadores
 
         /// <summary>
         /// Dos productos son iguales si comparten el mismo código de barras
@@ -53,7 +85,14 @@ namespace Entidades_2018
         /// <returns></returns>
         public static bool operator ==(Producto v1, Producto v2)
         {
-            return (v1.codigoDeBarras == v2.codigoDeBarras);
+            bool retorno = false;
+
+            if(v1.codigoDeBarras == v2.codigoDeBarras)
+            {
+                retorno = true;
+            }
+
+            return retorno;
         }
         /// <summary>
         /// Dos productos son distintos si su código de barras es distinto
@@ -63,7 +102,19 @@ namespace Entidades_2018
         /// <returns></returns>
         public static bool operator !=(Producto v1, Producto v2)
         {
-            return (v1.codigoDeBarras == v2.codigoDeBarras);
+            return !(v1 == v2);
         }
+
+        #endregion
+
+        #region Tipos Anidados
+
+        public enum EMarca
+        {
+            Serenisima, Campagnola, Arcor, Ilolay, Sancor, Pepsico
+        }
+
+        #endregion
+
     }
 }
