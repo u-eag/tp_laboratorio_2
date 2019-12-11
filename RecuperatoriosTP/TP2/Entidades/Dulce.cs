9575d9cb0b800a7f27c6e2cbe7ab9e76ffe5c1f1
Anto <eagualco@protonmail.com>
2019-12-11 16:33:57 -0300

    update clase Dulce

--- RecuperatoriosTP/TP2/Entidades/Dulce.cs
+++ RecuperatoriosTP/TP2/Entidades/Dulce.cs
@@ -8,32 +8,57 @@ namespace Entidades_2018
 {
     public class Dulce : Producto
     {
-        public Dulce(EMarca marca, string patente, ConsoleColor color)
-        {
-        }
+        #region Propiedades
 
         /// <summary>
         /// Los dulces tienen 80 calorías
         /// </summary>
-        protected short CantidadCalorias
+        protected override short CantidadCalorias
         {
             get
             {
-                return 0;
+                return 80;
             }
         }
 
-        private override sealed string Mostrar()
+        #endregion
+
+        #region Constructores
+
+        /// <summary>
+        /// Constructor que recibe 3 parámetros y llama al constructor de la clase base
+        /// </summary>
+        /// <param name="marca"></param>
+        /// <param name="patente"></param>
+        /// <param name="color"></param>
+        public Dulce(EMarca marca, string patente, ConsoleColor color)
+            : base(patente, marca, color)
+        {
+
+        }
+
+        #endregion
+
+        #region Métodos
+
+        /// <summary>
+        /// Publica todos los datos del Dulce.
+        /// </summary>
+        /// <returns></returns>
+        public override string Mostrar()
         {
             StringBuilder sb = new StringBuilder();
 
             sb.AppendLine("DULCE");
-            sb.AppendLine(this.Mostrar());
-            sb.AppendLine("CALORIAS : {0}", this.CantidadCalorias);
+            sb.AppendLine(base.Mostrar()); // llamo al método Mostrar de la base 
+            sb.AppendFormat("CALORIAS : {0}", this.CantidadCalorias);
             sb.AppendLine("");
             sb.AppendLine("---------------------");
 
-            return sb;
+            return sb.ToString();
         }
+
+        #endregion
+
     }
 }
