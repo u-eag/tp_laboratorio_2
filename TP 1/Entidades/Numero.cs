0b48c7ee1168079b85ffa2ac16369a92d41618f0
E, Antonella Gualco <Eliana.A.Gualco@gds.ey.com>
2020-04-16 02:55:31 -0300

    first commit

---
+++ TP 1/Entidades/Numero.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    public class Numero
+    {
+        #region Fields
+
+        private double numero;
+
+        #endregion
+
+        #region Properties
+
+        private string SetNumero
+        {
+            set 
+            {
+                
+            }
+        }
+
+        #endregion
+
+        #region Constructors
+
+        public Numero()
+        {
+
+        }
+
+        public Numero(double numero)
+        {
+
+        }
+
+        public Numero(string numero)
+        {
+
+        }
+
+        #endregion
+
+        #region Methods
+
+        private double ValidarNumero(string strNumero)
+        {
+            double retorno;
+
+            return retorno;
+        }
+
+        public string BinarioDecimal(string binario)
+        {
+            string retorno;
+
+            return retorno;
+        }
+
+        public string DecimalBinario(string numero)
+        {
+            string retorno;
+
+            return retorno;
+        }
+
+        public double DecimalBinario(double numero)
+        {
+            double retorno;
+
+            return retorno;
+        }
+
+        #endregion
+
+        #region Operators
+
+        public static double operator +(Numero n1, Numero n2)
+        {
+            double retorno;
+
+            return retorno;
+        }
+
+        public static double operator -(Numero n1, Numero n2)
+        {
+            double retorno;
+
+            return retorno;
+        }
+
+        public static double operator /(Numero n1, Numero n2)
+        {
+            double retorno;
+
+            return retorno;
+        }
+
+        public static double operator *(Numero n1, Numero n2)
+        {
+            double retorno;
+
+            return retorno;
+        }
+
+        #endregion
+    }
+}
