9f73b3b14561505a0d319704752d229829ed60fa
E, Antonella Gualco <Eliana.A.Gualco@gds.ey.com>
2020-04-19 17:07:21 -0300

    form design finished

---
+++ TP 1/MiCalculadora/FormCalculadora.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Entidades;
+
+namespace MiCalculadora
+{
+    public partial class FormCalculadora : Form
+    {
+        public FormCalculadora()
+        {
+            InitializeComponent();
+        }
+
+        #region Buttons
+        private void btnOperar_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnLimpiar_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnCerrar_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnConvertirABinario_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnConvertirADecimal_Click(object sender, EventArgs e)
+        {
+
+        }
+        #endregion
+
+        #region Methods
+        private void Limpiar()
+        {
+
+        }
+
+        private static double Operar(string numero1, string numero2, string operador)
+        {
+            return 0;
+        }
+        #endregion
+    }
+}
